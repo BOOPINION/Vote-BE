f727593749d5b53c9b8ef39a34b9030dbc064b0d
nowChae <nowchae23@gmail.com>
2024-05-06 18:56:22 +0900

    feat: 루트 페이지 DI 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,28 +3,28 @@ import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AuthModule } from "./feature/auth/auth.module";
 import { VoteModule } from "./feature/vote/vote.module";
-
-
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 /**
  * Root module
  */
 @Module({
-    imports: [
-        ConfigModule.forRoot({ isGlobal: true }),
-        TypeOrmModule.forRoot({
-            type: "mysql",
-            host: process.env.DB_HOST,
-            port: Number(process.env.DB_PORT),
-            username: process.env.DB_USERNAME,
-            password: process.env.DB_PASSWORD,
-            database: process.env.DB_NAME,
-            entities: [ "./model/db/*" ]
-        }),
-        AuthModule,
-        VoteModule
-    ],
-    controllers: [],
-    providers: []
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ["./model/db/*"],
+    }),
+    AuthModule,
+    VoteModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
