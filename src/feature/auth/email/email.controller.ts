83743fba816e7ce4775c7c1d3490ea94a0b21050
nowChae <nowchae23@gmail.com>
2024-05-13 00:32:20 +0900

    feat: email 전송 메소드 구현

--- src/feature/auth/email/email.controller.ts
+++ src/feature/auth/email/email.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get, Post } from "@nestjs/common";
+import { EmailService } from "./email.service";
 
 @Controller("auth/signup/email")
 export class EmailController {
+    constructor(private readonly emailService: EmailService) {}
+
     @Get()
     test() {
-        return "auth/signup/email";
+        this.emailService.sendEmail();
     }
 
     @Post("verify")
--- src/feature/auth/email/email.service.ts
+++ src/feature/auth/email/email.service.ts
@@ -1,4 +1,23 @@
+import { MailerService } from "@nestjs-modules/mailer";
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class EmailService {}
+export class EmailService {
+    constructor(private readonly mailerService: MailerService) {}
+
+    async sendEmail() {
+        try {
+            await this.mailerService.sendMail({
+                to: "testEmail",
+                from: process.env.MAIL_HOST,
+                subject: "test 제목",
+                text: "test 텍스트",
+                html: "<b>test<b>"
+            });
+
+            return true;
+        } catch (e) {
+            throw new Error(`Error in sendEmail method: ${e.message}`);
+        }
+    }
+}
