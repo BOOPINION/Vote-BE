5815cb5f038c9680292ee10522b5f9435031a32a
nowChae <nowchae23@gmail.com>
2024-05-31 22:54:52 +0900

    feat: get comment, post comment 기본 세팅

--- src/feature/votes/comments/comments.controller.ts
+++ src/feature/votes/comments/comments.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import {
+    Controller,
+    Get,
+    HttpException,
+    HttpStatus,
+    Param,
+    Post,
+    Headers
+} from "@nestjs/common";
 import { CommentsService } from "./comments.service";
 import { GetCommentsResponseDto } from "../dto/GetCommentsResponse.dto";
 
@@ -9,5 +17,18 @@ export class CommentsController {
     @Get(":voteId/comments")
     public async getComments(@Param("voteId") voteId: number) {
         return this.commentsService.getComments(voteId);
+    }
+
+    @Post(":voteId/comments")
+    public async createComment(
+    @Headers("Authorization") tokenHeader: string,
+        @Param("voteId") voteId: number
+    ) {
+        if (!tokenHeader || !tokenHeader.startsWith("Bearer ")) throw new HttpException("Token not provided", HttpStatus.UNAUTHORIZED);
+
+        const token = tokenHeader.replace("Bearer ", "");
+
+        await this.commentsService.createComment();
+
     }
 }
